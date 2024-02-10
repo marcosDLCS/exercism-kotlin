c29d78d1dc9a06db34e85936c92aa9ea7dae7a33
Marcos de la Calle <marcos.dlcs@gmail.com>
2024-02-10 11:23:51 +0100

    Solve resistor-color exercise

--- crypto-square/src/main/kotlin/CryptoSquare.kt
+++ crypto-square/src/main/kotlin/CryptoSquare.kt
@@ -1,12 +1,24 @@
+import java.util.*
+import kotlin.math.sqrt
+
 object CryptoSquare {
 
     fun ciphertext(plaintext: String): String {
-        TODO("Implement the function to complete the task")
+
+        // Test 01: empty plaintext results in empty ciphertext
+        if (plaintext.isEmpty()) return ""
+
+        // Test 02: letters are lower cased during encryption
+        // Test 03: spaces are removed during encryption
+        // Test 04: punctuation is removed during encryption
+        val normalizedText = plaintext.normalize()
+
+        return normalizedText
     }
 
     private fun String.normalize(): String {
-        return this.toLowerCase().replace(Regex(""), "")
+        return this
+            .lowercase(Locale.getDefault())
+            .replace(Regex("[\\s\\p{P}]"), "")
     }
-
-
 }
--- resistor-color/src/main/kotlin/ResistorColor.kt
+++ resistor-color/src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,24 @@
 object ResistorColor {
 
     fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
+        return Color.valueOf(input.uppercase()).value
     }
 
     fun colors(): List<String> {
-        TODO("Implement this to complete the task")
+        return Color.values().map { c -> c.name.lowercase() }
+    }
+
+    enum class Color(val value: Int) {
+        BLACK(0),
+        BROWN(1),
+        RED(2),
+        ORANGE(3),
+        YELLOW(4),
+        GREEN(5),
+        BLUE(6),
+        VIOLET(7),
+        GREY(8),
+        WHITE(9),
     }
 
 }
