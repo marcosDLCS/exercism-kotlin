573a20ec3c4309e59a58367b9a7b7662b20f3131
Marcos de la Calle <marcos.dlcs@gmail.com>
2024-02-10 18:14:59 +0100

    Put all the code in the same file for resistor-color-duo

--- resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
+++ resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -4,3 +4,16 @@ object ResistorColorDuo {
         return colors.slice(0..1).joinToString(separator = "") { color -> color.value.toString() }.toInt()
     }
 }
+
+enum class Color(val value: Int) {
+    BLACK(0),
+    BROWN(1),
+    RED(2),
+    ORANGE(3),
+    YELLOW(4),
+    GREEN(5),
+    BLUE(6),
+    VIOLET(7),
+    GREY(8),
+    WHITE(9),
+}
