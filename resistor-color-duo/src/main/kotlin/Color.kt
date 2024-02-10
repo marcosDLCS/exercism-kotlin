4938caa4f4022f56d5a94f81c05ab5ee8ff63124
Marcos de la Calle <marcos.dlcs@gmail.com>
2024-02-10 18:21:31 +0100

    Use enum ordinals for resistor-color-duo

---
+++ resistor-color-duo/src/main/kotlin/Color.kt
@@ -0,0 +1,12 @@
+enum class Color {
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE,
+}
--- resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
+++ resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -1,19 +1,6 @@
 object ResistorColorDuo {
 
     fun value(vararg colors: Color): Int {
-        return colors.slice(0..1).joinToString(separator = "") { color -> color.value.toString() }.toInt()
+        return colors.slice(0..1).joinToString(separator = "") { color -> color.ordinal.toString() }.toInt()
     }
-}
-
-enum class Color(val value: Int) {
-    BLACK(0),
-    BROWN(1),
-    RED(2),
-    ORANGE(3),
-    YELLOW(4),
-    GREEN(5),
-    BLUE(6),
-    VIOLET(7),
-    GREY(8),
-    WHITE(9),
 }
