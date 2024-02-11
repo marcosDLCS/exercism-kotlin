7230ced0cbefd7e189b6eb85fd8c73947865a393
Marcos de la Calle <marcos.dlcs@gmail.com>
2024-02-11 13:21:02 +0100

    Solve resistor-color-trio exercise

--- resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -1,6 +1,16 @@
+import kotlin.math.log10
+import kotlin.math.pow
+
 object ResistorColorTrio {
 
-    fun text(vararg input: Color): String {
-        TODO("Implement this to complete the task")
+    fun text(vararg colors: Color): String {
+
+        val inputValue = colors.take(2).joinToString("") { it.ordinal.toString() }.toInt()
+        val addedZeros = colors[2].ordinal
+        val resistorValue = inputValue * 10.0.pow(addedZeros)
+        val unit = Unit.values()[log10(resistorValue).toInt().div(3)]
+
+        return "${(resistorValue / 1000.0.pow(unit.ordinal)).toInt()} ${unit.name.lowercase()}"
     }
+
 }
