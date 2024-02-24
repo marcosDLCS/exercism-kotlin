90822f585408664892943f432cdb0d7665781600
Marcos de la Calle <marcos.dlcs@gmail.com>
2024-02-24 17:19:16 +0100

    Solve perfect-numbers exercise

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -1,8 +1,16 @@
-
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
-fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
+fun classify(n: Int): Classification {
+
+    require(n > 0)
+
+    val sum = (1 until n).filter { n % it == 0 }.sum()
+
+    return when {
+        sum < n -> Classification.DEFICIENT
+        sum > n -> Classification.ABUNDANT
+        else -> Classification.PERFECT
+    }
 }
