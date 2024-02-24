a10183dbea52968b00a773e6ce3d811b3b1922ad
Marcos de la Calle <marcos.dlcs@gmail.com>
2024-02-24 12:01:00 +0100

    Solve protein-translation exercise

--- protein-translation/src/main/kotlin/ProteinTranslation.kt
+++ protein-translation/src/main/kotlin/ProteinTranslation.kt
@@ -1,3 +1,21 @@
 fun translate(rna: String?): List<String> {
-    TODO("Implement this function to complete the task")
+    if (rna == null) return emptyList()
+
+    val codons = rna.chunked(3).map {
+        when (it) {
+            "AUG" -> "Methionine"
+            "UUU", "UUC" -> "Phenylalanine"
+            "UUA", "UUG" -> "Leucine"
+            "UCU", "UCC", "UCA", "UCG" -> "Serine"
+            "UAU", "UAC" -> "Tyrosine"
+            "UGU", "UGC" -> "Cysteine"
+            "UGG" -> "Tryptophan"
+            "UAA", "UAG", "UGA" -> "STOP"
+            else -> "INVALID"
+        }
+    }.takeWhile { item -> item != "STOP" }
+
+    require(!(codons.contains("INVALID"))) { "Invalid codon" }
+
+    return codons
 }
